4908ca996f789792fec1daaed3c9577cefff0b1c
akshit11318 <vvakshit11318@gmail.com>
2026-03-23 10:04:01 +0530

    feat: initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { execSync } from 'node:child_process'
+import path from 'path'
 
-let buildId = globalThis.process?.env?.VITE_BUILD_ID
+let buildId = globalThis.process ? .env ? .VITE_BUILD_ID
 if (!buildId) {
   try {
     buildId = execSync('git rev-parse --short HEAD').toString().trim()
@@ -14,6 +15,11 @@ if (!buildId) {
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   define: {
     'import.meta.env.VITE_BUILD_ID': JSON.stringify(buildId),
   },
